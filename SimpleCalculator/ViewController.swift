cc3fb3a3e8d447837019ad0ba6a6e7a1f8a84b3f
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2020-12-09 09:20:43 -0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by João Victor Ipirajá de Alencar on 09/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
